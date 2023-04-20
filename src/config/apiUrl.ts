d032d9afc426272a33e63a6f3f447e9438d0d19d
Alexis Graff <alexisgraff123@gmail.com>
2023-04-20 20:37:09 -0300

    Configurando modo produccion

--- src/config/apiUrl.ts
+++ src/config/apiUrl.ts
@@ -1,1 +1,12 @@
-export const API_URL = 'http://localhost:8080';
+let API;
+const MODE = import.meta.env.MODE;
+const DEVELOPMENT_URL = import.meta.env.VITE_APP_URL_DEVELOPMENT;
+const PRODUCTION_URL = import.meta.env.VITE_APP_URL_PRODUCTION;
+
+if (MODE === 'DEVELOPMENT') {
+    API = DEVELOPMENT_URL;
+} else {
+    API = PRODUCTION_URL;
+}
+
+export const API_URL = API;
